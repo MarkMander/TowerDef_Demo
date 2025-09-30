6b6d1a633633145a8028102ec5f2381a0425207a
MarkMander <markmanderson99@gmail.com>
2025-09-30 15:59:53 -0400

    pull enemyspawn point out of grid manager, make a prefab that is manually placed in scene. Note to self: should adjust camera speed to be relative to zoom so it's not super fast when zoomed in

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -6,18 +6,18 @@ public class GridManager : MonoBehaviour
 {
     public Tile tile;
     public int height, width;
-    public Transform cameraTransform;
-    public float spwnX = -5;
-    public EnemySpwn enemySpwnPt;
+    //public Transform cameraTransform;
+    //public float spwnX = -5;
+    //public EnemySpwn enemySpwnPt;
 
     
 
     public Dictionary<Vector2, Tile> GenerateGrid()
     {
         Dictionary<Vector2, Tile> tileDict = new Dictionary<Vector2, Tile>();
-        for (int i = 0; i < width; i++) 
+        for (int i = -width/2; i < width/2; i++) 
         {
-            for (int j = 0; j < height; j++)
+            for (int j = -height/2; j < height/2; j++)
             {
                 var spwnTile = Instantiate(tile,new Vector3(i,j), Quaternion.identity);
                 spwnTile.name = $"Tile {i}x{j}";
@@ -26,9 +26,9 @@ public class GridManager : MonoBehaviour
         }
         Debug.Log("Grid Generated");
 
-        Instantiate(enemySpwnPt, new Vector3(spwnX,(float)height / 2 - 0.5f), Quaternion.identity);
+        //Instantiate(enemySpwnPt, new Vector3(spwnX,(float)height / 2 - 0.5f), Quaternion.identity);
 
-        cameraTransform.position = new Vector3((float)width / 2 - 0.5f, (float)height / 2 - 0.5f, -10);
+        //cameraTransform.position = new Vector3((float)width / 2 - 0.5f, (float)height / 2 - 0.5f, -10);
         return tileDict;
     }
 }
